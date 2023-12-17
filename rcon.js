024ca665cccccde619be1473f54540535044a362
Oskru <oskrupaa@gmail.com>
2023-12-17 12:18:19 +0100

    Add additional rcon error handling and retrying

--- rcon.js
+++ rcon.js
@@ -30,6 +30,26 @@ rconClient.on('error', (err) => {
 
 rconClient.connect();
 
+const MAX_RETRIES = 3;
+let retryCount = 0;
+
 export async function sendRconCommand(command) {
-  rconClient.send(command);
+  try {
+    rconClient.send(command);
+    console.log(`Command ${command} sent successfully.`);
+    return;
+  } catch (error) {
+    if (error.name === 'ECONNRESET' && retryCount < MAX_RETRIES) {
+      retryCount++;
+      console.log(
+        `Retrying sending rcon command (${retryCount}/${MAX_RETRIES})...`,
+      );
+
+      sendRconCommandWithRetry(command);
+    } else {
+      retryCount = 0;
+      console.error(`Error sending RCON command: ${error.message}`);
+      throw error; // Propagate the error if it's not an ECONNRESET or max retries reached
+    }
+  }
 }
